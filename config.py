715ce41c78316121487e4fe3f6d1650c245daad7
yacinedexter <yacine.rsd2@gmail.com>
2018-04-08 11:33:30 -0400

    Add files via upload

--- config.py
+++ config.py
@@ -75,7 +75,7 @@ class Config():
     filename_test = "data/coNLL/eng/eng.testb.iob"
     filename_train = "data/coNLL/eng/eng.train.iob"
 
-    #filename_dev = filename_test = filename_train = "data/test.txt" # test
+    filename_dev = filename_test = filename_train = "data/test.txt" # test
 
     max_iter = None # if not None, max number of examples in Dataset
 
