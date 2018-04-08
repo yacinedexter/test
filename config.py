21a8ffd6dc94fd44db67112ba2b0c7609e2b0b86
yacinedexter <yacine.rsd2@gmail.com>
2018-04-08 11:20:40 -0400

    Add files via upload

--- config.py
+++ config.py
@@ -88,7 +88,7 @@ class Config():
     train_embeddings = False
     nepochs          = 15
     dropout          = 0.5
-    batch_size       = 20
+    batch_size       = 10
     lr_method        = "adam"
     lr               = 0.001
     lr_decay         = 0.9
@@ -97,7 +97,7 @@ class Config():
 
     # model hyperparameters
     hidden_size_char = 100 # lstm on chars
-    hidden_size_lstm = 300 # lstm on word embeddings
+    hidden_size_lstm = 50 # lstm on word embeddings
 
     # NOTE: if both chars and crf, only 1.6x slower on GPU
     use_crf = True # if crf, training is 1.7x slower on CPU
